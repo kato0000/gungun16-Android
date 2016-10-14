ea3cc4a24d88d51dd4ba1395fdec42c1fc3bd561
kato <qwerty.pj0505@gmail.com>
2016-10-14 15:08:08 +0900

    SelectActivityの作成

--- app/src/main/java/com/example/tech/gungun16/SelectActivity.java
+++ app/src/main/java/com/example/tech/gungun16/SelectActivity.java
@@ -1,7 +1,10 @@
 package com.example.tech.gungun16;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectActivity extends Activity {
 
@@ -9,5 +12,22 @@ public class SelectActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+        Button button = (Button) findViewById(R.id.buttonS);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onActiveClick(view);
+            }
+        });
+    }
+
+    public void onActiveClick(View view){
+        switch (view.getId()){
+            case R.id.buttonS:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
